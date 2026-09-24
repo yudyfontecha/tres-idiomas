4d77f4dcd9d41419bc2f1de615f33363d59d59ff
yudyfontecha <yudyfontecha@gmail.com>
2026-09-23 22:41:01 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+/* Tres idiomas - service worker */
+var CACHE = "tres-idiomas-v1";
+var SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(SHELL).catch(function(){}); }));
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.map(function(k){ if(k !== CACHE) return caches.delete(k); }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener("fetch", function(e){
+  var req = e.request;
+  if(req.method !== "GET") return;
+  var url = new URL(req.url);
+  if(url.origin !== self.location.origin) return;   // el traductor y las fuentes van directo a la red
+  e.respondWith(
+    fetch(req).then(function(res){
+      var copy = res.clone();
+      caches.open(CACHE).then(function(c){ c.put(req, copy); }).catch(function(){});
+      return res;
+    }).catch(function(){
+      return caches.match(req).then(function(hit){ return hit || caches.match("./index.html"); });
+    })
+  );
+});
